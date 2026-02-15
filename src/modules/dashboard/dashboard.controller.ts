2be3b52228fdcf69a68752b31e4cc6ab0cd8d9db
Adeeb-06 <mohammadadeeb886@gmail.com>
2026-02-15 12:31:36 +0600

    Dashboard Report

--- src/modules/dashboard/dashboard.controller.ts
+++ src/modules/dashboard/dashboard.controller.ts
@@ -4,7 +4,7 @@ import DashboardService from "./dashboard.services";
 export const getDashboardData = async (req: Request, res: Response) => {
   try {
     const { email } = req.params as { email: string };
-    const { month, year } = req.query 
+    const { month, year } = req.query;
 
     const balance = await DashboardService.getBalance(email);
     const totalIncome = await DashboardService.getTotalIncomeByMonth(
@@ -17,7 +17,25 @@ export const getDashboardData = async (req: Request, res: Response) => {
       Number(month),
       Number(year),
     );
-    return res.status(200).json({ balance, totalIncome, totalExpense });
+    const totalTransaction = await DashboardService.getTotalTransactionByMonth(
+      email,
+      Number(month),
+      Number(year),
+    );
+    const topExpenseCategory = await DashboardService.getTopExpenseCategory(
+      email,
+      Number(month),
+      Number(year),
+    );
+    return res
+      .status(200)
+      .json({
+        balance,
+        totalIncome,
+        totalExpense,
+        totalTransaction,
+        topExpenseCategory,
+      });
   } catch (error) {
     console.log(error);
     return res.status(500).json({ message: "Internal server error" });
--- src/modules/dashboard/dashboard.services.ts
+++ src/modules/dashboard/dashboard.services.ts
@@ -1,63 +1,126 @@
-import BalanceModel from "../balance/balance.model"
-import ExpenseModel from "../expense/expense.model"
-import User from "../user/user.model"
+import BalanceModel from "../balance/balance.model";
+import ExpenseModel from "../expense/expense.model";
+import User from "../user/user.model";
 
 const getBalance = async (email: string) => {
-    const user = await User.findOne({email })
-    return user?.balance
-}
+  const user = await User.findOne({ email });
+  return user?.balance;
+};
 
-const getTotalIncomeByMonth = async (email: string , month: number , year:number) => {
-    console.log(year, month , email)
-    const res = await BalanceModel.aggregate([
-        {
-            $match: {
-                userEmail: email,
-                date: {
-                    $gte: new Date(year, month - 1, 1),
-                    $lt: new Date(year, month, 1)
-                }
-            }
+const getTotalIncomeByMonth = async (
+  email: string,
+  month: number,
+  year: number,
+) => {
+  console.log(year, month, email);
+  const res = await BalanceModel.aggregate([
+    {
+      $match: {
+        userEmail: email,
+        date: {
+          $gte: new Date(year, month - 1, 1),
+          $lt: new Date(year, month, 1),
         },
-        {
-            $group: {
-                _id: null,
-                totalIncome: { $sum: "$amount" }
-            }
-        }
-    ])
-    console.log(res)
-    return res[0]?.totalIncome
-}
+      },
+    },
+    {
+      $group: {
+        _id: null,
+        totalIncome: { $sum: "$amount" },
+      },
+    },
+  ]);
+  console.log(res);
+  return res[0]?.totalIncome;
+};
 
-const getTotalExpenseByMonth = async (email: string , month: number , year:number) => {
-    console.log(year , month , email)
-    const res = await ExpenseModel.aggregate([
-        {
-            $match: {
-                userEmail: email,
-                date: {
-                    $gte: new Date(year, month - 1, 1),
-                    $lt: new Date(year, month, 1)
-                }
-            }
+const getTotalExpenseByMonth = async (
+  email: string,
+  month: number,
+  year: number,
+) => {
+  console.log(year, month, email);
+  const res = await ExpenseModel.aggregate([
+    {
+      $match: {
+        userEmail: email,
+        date: {
+          $gte: new Date(year, month - 1, 1),
+          $lt: new Date(year, month, 1),
         },
-        {
-            $group: {
-                _id: null,
-                totalExpense: { $sum: "$amount" }
-            }
-        }
-    ])
-    console.log(res)
-    return res[0]?.totalExpense
-}
+      },
+    },
+    {
+      $group: {
+        _id: null,
+        totalExpense: { $sum: "$amount" },
+      },
+    },
+  ]);
+  console.log(res);
+  return res[0]?.totalExpense;
+};
 
+const getTotalTransactionByMonth = async (
+  email: string,
+  month: number,
+  year: number,
+) => {
+  console.log(year, month, email);
+  const res = await ExpenseModel.aggregate([
+    {
+      $match: {
+        userEmail: email,
+        date: {
+          $gte: new Date(year, month - 1, 1),
+          $lt: new Date(year, month, 1),
+        },
+      },
+    },
+  ]);
+  console.log(res.length);
+  return res.length;
+};
+
+const getTopExpenseCategory = async (
+  email: string,
+  month: number,
+  year: number,
+) => {
+  console.log(year, month, email);
+  const res = await ExpenseModel.aggregate([
+    {
+      $match: {
+        userEmail: email,
+        date: {
+          $gte: new Date(year, month - 1, 1),
+          $lt: new Date(year, month, 1),
+        },
+      },
+    },
+    {
+      $group: {
+        _id: "$category",
+        amount: { $sum: "$amount" },
+      },
+    },
+  ]);
+  console.log(res, "resss");
+  if (res.length === 0) {
+    return { category: "No Category", amount: 0 };
+  }
+  const sortedCategories = res.sort((a: any, b: any) => b.amount - a.amount);
+  const topCategory = sortedCategories[0];
+  console.log(topCategory);
+  return { category: topCategory._id, amount: topCategory.amount };
+};
 
 const DashboardService = {
-    getBalance,
-    getTotalIncomeByMonth,
-    getTotalExpenseByMonth
-}
+  getBalance,
+  getTotalIncomeByMonth,
+  getTotalExpenseByMonth,
+  getTotalTransactionByMonth,
+  getTopExpenseCategory,
+};
 
-export default DashboardService
+export default DashboardService;
